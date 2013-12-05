aa7d3adc502841d863abedce643e568d916c0fe4
eabrawer <ericbrawer@gmail.com>
2013-12-04 22:57:58 -0500

    First commit, installing Rspec/FG/Capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budo::Application.config.secret_key_base = '3e452b530c2abad36643590591f2488619857efe0d10348f45c292a8b90a90dd81078c2eb9b20d6caf655c45d25fea207f55c80166ce8bc5b3419498c15f4acf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Budo::Application.config.session_store :cookie_store, key: '_budo_session'
